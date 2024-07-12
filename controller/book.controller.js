4d26bb4440d105d8ba69546180896e684a49eda5
diyawebearl <diya.webearl@gmail.com>
2024-07-12 12:14:07 +0530

    Update book.controller.js changed email to auth id

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,19 +1,265 @@
+// const Author = require('../model/author.model.js');
+// const Book = require('../model/book.model.js');
+// const Category = require('../model/category.model.js');
+
+// const addBook = async (req, res) => {
+//     const { book_title, book_description, book_page, status, category_name } = req.body;
+//     const file = req.file;
+
+//     try {
+//           if (!email) {
+//             return res.status(400).send({ error: 'Email is required' });
+//         }
+//         const author = await Author.findOne({ email });
+//         if (!author) {
+//             return res.status(400).send({ error: 'Author not found' });
+//         }
+
+//         const category = await Category.findOne({ category_name });
+//         if (!category) {
+//             return res.status(400).send({ error: 'Category not found' });
+//         }
+
+//         if (!file) {
+//             return res.status(400).send({ error: 'Book cover photo is required' });
+//         }
+
+//         const newBook = new Book({
+//             category_id: category._id,
+//             author_id: author._id,
+//             book_title,
+//             book_description,
+//             book_cover_photo: file.filename,
+//             book_page,
+//             status,
+//         });
+
+//         await newBook.save();
+//         return res.status(200).send(newBook);
+//     } catch (error) {
+//         return res.status(400).send({ error: error.message });
+//     }
+// }
+
+// const deleteBook = async (req, res) => {
+//     const { id } = req.params;
+
+//     try {
+//         const book = await Book.findByIdAndDelete(id);
+//         if (!book) {
+//             return res.status(404).send({ error: 'Book not found' });
+//         }
+//         return res.status(200).send({ message: 'Book deleted successfully' });
+//     } catch (error) {
+//         return res.status(400).send({ error: error.message });
+//     }
+// }
+
+// const displayAllBook = async (req, res) => {
+//     try {
+//         const books = await Book.aggregate([
+//             {
+//                 $lookup: {
+//                     from: 'authors', // Collection name in MongoDB
+//                     localField: 'author_id', // Field in the Book collection
+//                     foreignField: '_id', // Field in the Author collection
+//                     as: 'authorDetails' // Name of the field to add the author details
+//                 }
+//             },
+//             {
+//                 $lookup: {
+//                     from: 'categories', // Collection name in MongoDB
+//                     localField: 'category_id', // Field in the Book collection
+//                     foreignField: '_id', // Field in the Category collection
+//                     as: 'categoryDetails' // Name of the field to add the category details
+//                 }
+//             },
+//             {
+//                 $unwind: '$authorDetails' // Unwind the array to get author details as an object
+//             },
+//             {
+//                 $project: {
+//                     _id: 1,
+//                     book_title: 1,
+//                     book_description: 1,
+//                     status: 1,
+//                     book_cover_photo: 1,
+//                     createdAt: 1,
+//                     updatedAt: 1,
+//                     'authorDetails.name': 1, 
+//                     'authorDetails.dob': 1,  
+//                     'authorDetails.city': 1, 
+//                     'authorDetails.state': 1, 
+//                     'authorDetails.country': 1, 
+//                     'authorDetails.gender': 1, 
+//                     'authorDetails.photo': 1, 
+//                     'authorDetails.email': 1, 
+//                     'authorDetails.mobile': 1,
+//                     'categoryDetails.category_name': 1 
+//                 }
+//             }
+//         ]);
+
+//         return res.status(200).send(books);
+//     } catch (error) {
+//         return res.status(400).send({ error: error.message });
+//     }
+// };
+
+
+// const editBook = async (req, res) => {
+//     const { id } = req.params;
+//     const { book_title, book_description, book_page, status, category_name, name } = req.body;
+//     const file = req.file;
+
+//     try {
+//         const updateFields = {
+//             book_title,
+//             book_description,
+//             book_page,
+//             status,
+//             category_name,
+//             name
+//         };
+//         let book = await Book.findById(id);
+//         if (!book) {
+//             return res.status(404).send({ error: 'Book not found' });
+//         }
+
+
+//         if (name) {
+//             const author = await Author.findOne({ name });
+//             if (!author) {
+//                 return res.status(400).send({ error: 'Author not found' });
+//             }
+//             updateFields.author_id = author._id;
+//         }
+
+//         if (category_name) {
+//             const category = await Category.findOne({ category_name });
+//             if (!category) {
+//                 return res.status(400).send({ error: 'Category not found' });
+//             }
+//             updateFields.category_id = category._id;
+//         }
+//         if (file) {
+//             updateFields.book_cover_photo = req.file.filename
+//         }
+
+//         let updatedBook = await Book.findByIdAndUpdate(id, updateFields, { new: true });
+
+//         return res.status(200).send(updatedBook);
+//     } catch (error) {
+//         return res.status(400).send({ error: error.message });
+//     }
+// }
+
+
+
+// const bookDetail = async (req, res) => {
+//     const { id } = req.params;
+//     const baseURL = "https://bookingreadingapp.onrender.com/uploads";
+//     try {
+//         const book = await Book.findById(id);
+//         if (!book) {
+//             return res.status(400).send({ message: "Book is not present with this ID" });
+//         }
+
+//         const bookWithFullURL = {
+//             ...book.toObject(), 
+//             book_cover_photo: baseURL + "/" + book.book_cover_photo
+//         };
+
+//         return res.status(200).send(bookWithFullURL);
+//     } catch (error) {
+//         return res.status(400).send({ error: error.message });
+//     }
+// };
+
+// module.exports = bookDetail;
+
+
+// const booksByAuthor = async (req, res) => {
+//     const { id } = req.params;
+//     const baseURL = "https://bookingreadingapp.onrender.com/uploads";
+//     try {
+//         const author = await Author.findById(id);
+//         if (!author) {
+//             return res.status(404).send({ error: 'Author not found' });
+//         }
+
+//         const books = await Book.aggregate([
+//             {
+//                 $match: { author_id: author._id }
+//             },
+//             {
+//                 $lookup: {
+//                     from: 'authors',
+//                     localField: 'author_id',
+//                     foreignField: '_id',
+//                     as: 'author'
+//                 }
+//             },
+//             {
+//                 $unwind: '$author'
+//             },
+//             {
+//                 $project: {
+//                     book_title: 1,
+//                     book_description: 1,
+//                     book_cover_photo: 1,
+//                     book_page: 1,
+//                     status: 1,
+//                     author: '$author.author_name'
+//                 }
+//             }
+//         ]);
+//         const booksWithFullURL = books.map(book => ({
+//             ...book,
+//             book_cover_photo: `${baseURL}/${book.book_cover_photo}`
+//         }));
+
+//         return res.status(200).json({
+//             message: "Book got successfully",
+//             books: booksWithFullURL
+//         });
+//     } catch (error) {
+//         return res.status(400).send({ error: error.message });
+//     }
+// }
+
+// module.exports = {
+//     addBook,
+//     deleteBook,
+//     displayAllBook,
+//     editBook,
+//     bookDetail,
+//     booksByAuthor
+// }
+
+
+
+
+
+
+
+
+
+
+
 const Author = require('../model/author.model.js');
 const Book = require('../model/book.model.js');
 const Category = require('../model/category.model.js');
 
 const addBook = async (req, res) => {
-    const { book_title, book_description, book_page, status, category_name } = req.body;
+    const { book_title, book_description, book_page, status, category_name, author_id } = req.body;
     const file = req.file;
 
-    // try {
-    //       if (!email) {
-    //         return res.status(400).send({ error: 'Email is required' });
-    //     }
-    //     const author = await Author.findOne({ email });
-    //     if (!author) {
-    //         return res.status(400).send({ error: 'Author not found' });
-    //     }
+    try {
+        const author = await Author.findById(author_id);
+        if (!author) {
+            return res.status(400).send({ error: 'Author not found' });
+        }
 
         const category = await Category.findOne({ category_name });
         if (!category) {
@@ -106,10 +352,9 @@ const displayAllBook = async (req, res) => {
     }
 };
 
-
 const editBook = async (req, res) => {
     const { id } = req.params;
-    const { book_title, book_description, book_page, status, category_name, name } = req.body;
+    const { book_title, book_description, book_page, status, category_name, author_id } = req.body;
     const file = req.file;
 
     try {
@@ -118,17 +363,15 @@ const editBook = async (req, res) => {
             book_description,
             book_page,
             status,
-            category_name,
-            name
         };
+
         let book = await Book.findById(id);
         if (!book) {
             return res.status(404).send({ error: 'Book not found' });
         }
 
-
-        if (name) {
-            const author = await Author.findOne({ name });
+        if (author_id) {
+            const author = await Author.findById(author_id);
             if (!author) {
                 return res.status(400).send({ error: 'Author not found' });
             }
@@ -142,8 +385,9 @@ const editBook = async (req, res) => {
             }
             updateFields.category_id = category._id;
         }
+
         if (file) {
-            updateFields.book_cover_photo = req.file.filename
+            updateFields.book_cover_photo = req.file.filename;
         }
 
         let updatedBook = await Book.findByIdAndUpdate(id, updateFields, { new: true });
@@ -153,8 +397,6 @@ const editBook = async (req, res) => {
         return res.status(400).send({ error: error.message });
     }
 }
-
-
 
 const bookDetail = async (req, res) => {
     const { id } = req.params;
@@ -175,9 +417,6 @@ const bookDetail = async (req, res) => {
         return res.status(400).send({ error: error.message });
     }
 };
-
-module.exports = bookDetail;
-
 
 const booksByAuthor = async (req, res) => {
     const { id } = req.params;
@@ -210,17 +449,18 @@ const booksByAuthor = async (req, res) => {
                     book_cover_photo: 1,
                     book_page: 1,
                     status: 1,
-                    author: '$author.author_name'
+                    author: '$author.name'
                 }
             }
         ]);
+
         const booksWithFullURL = books.map(book => ({
             ...book,
             book_cover_photo: `${baseURL}/${book.book_cover_photo}`
         }));
 
         return res.status(200).json({
-            message: "Book got successfully",
+            message: "Books retrieved successfully",
             books: booksWithFullURL
         });
     } catch (error) {
@@ -236,3 +476,4 @@ module.exports = {
     bookDetail,
     booksByAuthor
 }
+
